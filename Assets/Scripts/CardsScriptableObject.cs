18ad98590e4fb75633b8ab87e3f29f47ff238825
Aymeric Lamboley <aymeric.lamboley@gmail.com>
2020-01-31 23:03:33 +0100

    so objects

---
+++ Assets/Scripts/CardsScriptableObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+public class CardsScriptableObject : ScriptableObject
+{
+    public CardData[] cards;
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/Create cards")]
+    static void CreateCards()
+    {
+        CardsScriptableObject data = CreateInstance<CardsScriptableObject>();
+
+        AssetDatabase.CreateAsset(data, "Assets/Scripts/Cards.asset");
+        EditorUtility.SetDirty(data);
+        AssetDatabase.SaveAssets();
+    }
+#endif
+}
+
+[Serializable]
+public class CardData
+{
+    public Sprite front;
+
+    public int a;
+    public int b;
+    public int c;
+}
---
+++ Assets/Scripts/SpellsScriptableObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+public class SpellsScriptableObject : ScriptableObject
+{
+    public SpellData[] spells;
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/Create spells")]
+    static void CreateSpells()
+    {
+        SpellsScriptableObject data = CreateInstance<SpellsScriptableObject>();
+
+        AssetDatabase.CreateAsset(data, "Assets/Scripts/Spells.asset");
+        EditorUtility.SetDirty(data);
+        AssetDatabase.SaveAssets();
+    }
+#endif
+}
+
+[Serializable]
+public class SpellData
+{
+    public Sprite front;
+
+    public int a;
+    public int b;
+    public int c;
+}
