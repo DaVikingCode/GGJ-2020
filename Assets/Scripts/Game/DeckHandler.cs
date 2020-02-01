88f9e386f1c2de45e5f16c2de17e028fb2176438
Théo <theo.chapelot@gmail.com>
2020-02-01 20:03:15 +0100

    Update DeckHandler.cs

--- Assets/Scripts/Game/DeckHandler.cs
+++ Assets/Scripts/Game/DeckHandler.cs
@@ -29,71 +29,13 @@ public class DeckHandler : MonoBehaviour
 
 		for (int i = 0; i < numberOfCards; i++)
 		{
-			int randomIndex = UnityEngine.Random.Range(0, cards.Count);
+			int randomIndex = Random.Range(0, cards.Count);
 			pool.Add(cards[randomIndex]);
 			cards.RemoveAt(randomIndex);
 		}
 
 		return new List<CardData>[] { pool, cards };
     }
-
-    /*public void initializeDeck()
-    {
-		List<CardData> initialPool = new List<CardData>();
-		List<CardData> remainingCards = new List<CardData>();
-
-        bool spellFound = false;
-
-		while (!spellFound)
-		{
-			deck = new List<CardData>();
-
-			List<CardData>[] result = getRandomCardsFromList(cardsSO.cards.ToList(), 4);
-			initialPool = result[0];
-			remainingCards = result[1];
-			deck.AddRange(initialPool);
-
-			List<SpellData> availableSpells = findPlayableSpells(deck);
-            if (availableSpells.Count > 0)
-            {
-                currentSpell = availableSpells[UnityEngine.Random.Range(0, availableSpells.Count)];
-                spellFound = true;
-            }
-		}
-
-		List<CardData> filler = getRandomCardsFromList(remainingCards, 3)[0];
-		deck.AddRange(filler);
-
-		deck.Shuffle();
-
-		_currentIndex = 0;
-		currentCard = deck[0];
-    }*/
-
-    /*List<SpellData> findPlayableSpells(List<CardData> deck)
-    {
-		List<SpellData> availableSpells = new List<SpellData>();
-
-		foreach (SpellData spell in spellsSO.spells)
-		{
-			int a = 0;
-			int b = 0;
-			int c = 0;
-
-			foreach (CardData card in deck)
-			{
-				a += card.a;
-				b += card.b;
-				c += card.c;
-			}
-			if (spell.minA <= a && a <= spell.maxA
-				&& spell.minB <= b && b <= spell.maxB
-				&& spell.minC <= c && c <= spell.maxC)
-				availableSpells.Add(spell);
-		}
-
-		return availableSpells;
-    }*/
 
 	public bool goToNextCard()
 	{
