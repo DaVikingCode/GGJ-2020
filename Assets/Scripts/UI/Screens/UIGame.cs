aab87f5233a6ec13d627dda471e6dccdbe2c1c2f
Viking Mac Dev <>
2020-02-01 16:51:13 +0100

    terrible pop animation for testing

--- Assets/Scripts/UI/Screens/UIGame.cs
+++ Assets/Scripts/UI/Screens/UIGame.cs
@@ -19,15 +19,33 @@ public class UIGame : BaseUIScreen
         switch(resourceIndex)
         {
             case 0:
+                StartCoroutine(PopResource(rA));
                 tA.text = value.ToString();
                 break;
             case 1:
+                StartCoroutine(PopResource(rB));
                 tB.text = value.ToString();
                 break;
             case 2:
+                StartCoroutine(PopResource(rC));
                 tC.text = value.ToString();
                 break;
         }
+    }
+
+    IEnumerator PopResource(Image img)
+    {
+        Vector3 scale1 = img.rectTransform.localScale;
+        Vector3 scaleB = new Vector3(1.4f, 1.4f, 1f);
+
+        float dur = 1f;
+        for(float time = 0f; time < dur; time += Time.deltaTime)
+        {
+            img.rectTransform.localScale = Vector3.LerpUnclamped(scale1, scaleB, Mathf.Cos(time * 0.02f));
+            yield return null;
+        }
+
+        img.rectTransform.localScale = scale1;
     }
 
 }
