ed9461b52f44aef38816904d39f94c11afb9f529
Viking Mac Dev <>
2020-02-02 12:58:55 +0100

    color change fix (allow for circular lerp on hue)

--- Assets/Scripts/UI/Screens/UIGame.cs
+++ Assets/Scripts/UI/Screens/UIGame.cs
@@ -11,35 +11,49 @@ public class UIGame : BaseUIScreen
     public UICard card;
 	public Slider slider;
 
+    public class UnclampedHSV
+    {
+        public float hue;
+        public float sat;
+        public float val;
+        public Color ToColor()
+        {
+            return Color.HSVToRGB(hue, sat, val);
+        }
+
+        public static UnclampedHSV fromColor(Color c)
+        {
+            UnclampedHSV res = new UnclampedHSV();
+            Color.RGBToHSV(c, out res.hue, out res.sat, out res.val);
+            return res;
+        }
+    }
+
     [HideInInspector]
     public bool animatingColors = false;
 
-    public void SetTarget(float hue, float lightness, float hueScale = 360f, float lightnessScale = 100f)
+    public void SetTarget(float hue, float lightness)
     {
-        Color c = Utils.GetColor(hue, lightness, hueScale, lightness);
+        Color c = Utils.GetColor(hue % 360f, lightness % 100f, 360f, 100f);
         frame.color = c;
     }
 
-    public void SetCurrent(float hue, float lightness, float hueScale = 360f, float lightnessScale = 100f)
+    public void SetCurrent(float hue, float lightness)
     {
-        Color to = Utils.GetColor(hue, lightness, hueScale, lightness);
-        Color from = background.color;
+        UnclampedHSV to = new UnclampedHSV()
+        {
+            hue = hue/360f,
+            val = lightness/100f
+        };
+        UnclampedHSV from = UnclampedHSV.fromColor(background.color);
 
         this.animatingColors = true;
         AnimateColorInHSLSpace(background, from, to, 0.5f);
     }
 
-    public Coroutine AnimateColorInHSLSpace(Image image, Color from, Color to, float duration)
+    public Coroutine AnimateColorInHSLSpace(Image image, UnclampedHSV from, UnclampedHSV to, float duration)
     {
-        float hA;
-        float sA;
-        float lA;
-        Color.RGBToHSV(from, out hA, out sA, out lA);
 
-        float hB;
-        float sB;
-        float lB;
-        Color.RGBToHSV(to, out hB, out sB, out lB);
 
         return GameManager.instance.animationManager.Animate(duration, (float t) =>
          {
@@ -47,14 +61,9 @@ public class UIGame : BaseUIScreen
              float s = 0.5f;
              float l = 0f;
 
-             if(hA == hB)
-             {
-                 hB = hA + 1f;
-             }
-
-             h = Mathf.LerpUnclamped(hA, hB, t);
+             h = Mathf.LerpUnclamped(from.hue, to.hue, t);
              //s = Mathf.LerpUnclamped(sA, sB, t);
-             l = Mathf.LerpUnclamped(lA, lB, t);
+             l = Mathf.LerpUnclamped(from.val, to.val, t);
 
              image.color = Color.HSVToRGB(Mathf.Repeat(h,1f), s, Mathf.Clamp01(l));
              return true;
