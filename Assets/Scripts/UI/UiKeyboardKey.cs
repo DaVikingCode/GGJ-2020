6a149edd8e30ae67e2f99848891ccea6c9d80fa6
Viking Mac Dev <>
2020-02-02 16:42:49 +0100

    - key nav

--- Assets/Scripts/UI/UiKeyboardKey.cs
+++ Assets/Scripts/UI/UiKeyboardKey.cs
@@ -8,9 +8,12 @@ public class UiKeyboardKey : MonoBehaviour
     Image img;
 	RectTransform rectTransform;
 
+    Color startColor;
+
     private void Awake()
     {
         img = GetComponent<Image>();
+        startColor = img.color;
 		rectTransform = GetComponent<RectTransform>();
     }
 
@@ -24,8 +27,12 @@ public class UiKeyboardKey : MonoBehaviour
 		Vector2 scaleStart = rectTransform.localScale;
 		Vector2 scaleEnd = direction ? new Vector2(1.3f, 1.3f) : new Vector2(1f,1f);
 
+        Color sColor = direction ? startColor : Color.grey;
+        Color eColor = direction ? Color.grey : startColor;
+
         yield return GameManager.instance.animationManager.Animate(0.2f,(float t )=> {
 
+            this.img.color = Color.Lerp(sColor, eColor, t);
 			this.rectTransform.localScale = Vector2.LerpUnclamped(scaleStart, scaleEnd, t);
             return true;
         }, AnimationManager.EASING.EASE_IN);
